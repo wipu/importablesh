7bad92872d446221061122800af9a30ddffe9ae5
Ville Oikarinen <ville@oikarinen.org>
2019-02-21 10:58:09 +0200

    xterm-title now only works in terminal

--- functions/xterm-title.sh
+++ functions/xterm-title.sh
@@ -1,4 +1,7 @@
 xterm-title() {
     TITLE=$1
-    printf "\033]0;$TITLE\007"
+    # only do it if running in terminal
+    # so we don't make a noisy text output when redirecting
+    # also, in some cases it would cause tecnical problems
+    [ -t 1 ] && printf "\033]0;$TITLE\007"
 }
