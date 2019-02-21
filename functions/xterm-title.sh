9d525289e29c6e2c7ee7fa5f16894da650939bfb
Ville Oikarinen <ville@oikarinen.org>
2019-02-21 11:14:28 +0200

    probable fix with set -eu in xterm-title

--- functions/xterm-title.sh
+++ functions/xterm-title.sh
@@ -3,5 +3,5 @@ xterm-title() {
     # only do it if running in terminal
     # so we don't make a noisy text output when redirecting
     # also, in some cases it would cause tecnical problems
-    [ -t 1 ] && printf "\033]0;$TITLE\007"
+    [ -t 1 ] && printf "\033]0;$TITLE\007" || true
 }
